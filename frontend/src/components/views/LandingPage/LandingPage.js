bd8a760aec955a959e06f48f20cee1b5e62f6737
Redonearth <wona23@gmail.com>
2021-01-30 19:21:52 +0900

    로그인 페이지 및 Redux 추가

--- frontend/src/components/views/LandingPage/LandingPage.js
+++ frontend/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,19 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
+const style = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100vh"
+};
+
 function LandingPage() {
   useEffect(() => {
     axios.get("/api/hello").then((res) => console.log(res.data));
   }, []);
-  return <div>LandingPage</div>;
+  return <div style={style}>시작 페이지</div>;
 }
 
 export default LandingPage;
--- frontend/src/components/views/LoginPage/LoginPage.js
+++ frontend/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-function LoginPage() {
-  return <div>LoginPage</div>;
+const style = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100vh"
+};
+
+const formStyle = {
+  display: "flex",
+  flexDirection: "column"
+};
+
+function LoginPage(props) {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onEmailChangeHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordChangeHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let loginForm = {
+      email,
+      password
+    };
+
+    dispatch(loginUser(loginForm)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push("/");
+      } else {
+        alert("Login Error.");
+      }
+    });
+  };
+
+  return (
+    <div style={style}>
+      <form style={formStyle} onSubmit={onSubmitHandler}>
+        <label>E-mail</label>
+        <input type="email" value={email} onChange={onEmailChangeHandler} />
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={onPasswordChangeHandler}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
