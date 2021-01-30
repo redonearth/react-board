987bb535b0411847335f454cc41b4c646b703a99
Redonearth <wona23@gmail.com>
2021-01-30 19:54:14 +0900

    로그아웃 버튼 추가

--- frontend/src/components/views/LandingPage/LandingPage.js
+++ frontend/src/components/views/LandingPage/LandingPage.js
@@ -5,15 +5,34 @@ const style = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
+  flexDirection: "column",
   width: "100%",
   height: "100vh"
 };
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((res) => console.log(res.data));
   }, []);
-  return <div style={style}>시작 페이지</div>;
+
+  const onLogoutClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃이 실패했습니다.");
+      }
+    });
+  };
+
+  return (
+    <>
+      <div style={style}>
+        <h2>시작 페이지</h2>
+        <button onClick={onLogoutClickHandler}>로그아웃</button>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
