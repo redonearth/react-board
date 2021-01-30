b3339f278fba37ee304720d13d1d1fcc26103c4b
Redonearth <wona23@gmail.com>
2021-01-30 12:52:15 +0900

    User Model 추가

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://redonearth:abcd1234@boilerplate.noibt.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello Express!"));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
