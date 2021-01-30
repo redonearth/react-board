8450dd896a921e8a821b521bcb47ccc9738ab95e
Redonearth <wona23@gmail.com>
2021-01-30 13:19:27 +0900

    MongoDB 설정 정보 추가

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://redonearth:abcd1234@boilerplate.noibt.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
   .then(() => console.log("MongoDB Connected!"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Express!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    });
+  });
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
