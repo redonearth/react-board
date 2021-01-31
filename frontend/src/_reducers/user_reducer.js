e7b7f8c757df110fa53e0365cbc81aebb5a814ca
Redonearth <wona23@gmail.com>
2021-01-31 21:50:30 +0900

    user_reducer 수정

--- frontend/src/_reducers/user_reducer.js
+++ frontend/src/_reducers/user_reducer.js
@@ -1,17 +1,16 @@
 import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "../_actions/types";
 
-export default (state = {}, action) => {
+const user_reducer = (state = {}, action) => {
   switch (action.type) {
     case LOGIN_USER:
       return { ...state, loginSuccess: action.payload };
-      break;
     case REGISTER_USER:
       return { ...state, register: action.payload };
-      break;
     case AUTH_USER:
       return { ...state, userData: action.payload };
-      break;
     default:
       return state;
   }
 };
+
+export default user_reducer;
