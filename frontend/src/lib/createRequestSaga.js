065d8b1b34f2d4a299950d53e66a98343a02c78e
Redonearth <wona23@gmail.com>
2021-02-18 21:00:16 +0900

    API Saga 생성

--- frontend/src/lib/createRequestSaga.js
+++ frontend/src/lib/createRequestSaga.js
@@ -1,10 +1,15 @@
 import { put, call } from "redux-saga/effects";
 import { startLoading, finishLoading } from "../modules/loading";
 
+export const createRequestActionTypes = (type) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return [type, SUCCESS, FAILURE];
+};
+
 export default function createRequestSaga(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
-
   return function* (action) {
     yield put(startLoading(type));
     try {
--- frontend/src/modules/auth.js
+++ frontend/src/modules/auth.js
@@ -1,8 +1,21 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
+import { takeLatest } from "redux-saga/effects";
+import createRequestSaga, {
+  createRequestActionTypes
+} from "../lib/createRequestSaga";
+import * as authAPI from "../lib/api/users";
 
 const CHANGE_FIELD = "auth/CHANGE_FIELD";
 const INITIALIZE_FORM = "auth/INITIALIZE_FORM";
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+  "auth/REGISTER"
+);
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+  "auth/LOGIN"
+);
 
 export const changeField = createAction(
   CHANGE_FIELD,
@@ -14,10 +27,27 @@ export const changeField = createAction(
 );
 
 export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+export const register = createAction(
+  REGISTER,
+  ({ username, email, password }) => ({ username, email, password })
+);
+export const login = createAction(LOGIN, ({ email, password }) => ({
+  email,
+  password
+}));
+
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+  yield takeLatest(REGISTER, registerSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
 
 const initialState = {
+  register: { username: "", email: "", password: "", passwordConfirm: "" },
   login: { email: "", password: "" },
-  register: { username: "", email: "", password: "", passwordConfirm: "" }
+  auth: null,
+  authError: null
 };
 
 const auth = handleActions(
@@ -28,8 +58,13 @@ const auth = handleActions(
       }),
     [INITIALIZE_FORM]: (state, { payload: form }) => ({
       ...state,
-      [form]: initialState[form]
-    })
+      [form]: initialState[form],
+      authError: null
+    }),
+    [REGISTER_SUCCESS]: () => ({}),
+    [REGISTER_FAILURE]: () => ({}),
+    [LOGIN_SUCCESS]: () => ({}),
+    [LOGIN_FAILURE]: () => ({})
   },
   initialState
 );
