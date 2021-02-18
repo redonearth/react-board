4cef601d6bddbb1eb689855747ab926f89cb64ed
Redonearth <wona23@gmail.com>
2021-02-19 01:05:17 +0900

    회원가입 로직 추가

--- frontend/src/components/auth/RegisterForm.js
+++ frontend/src/components/auth/RegisterForm.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeField, initializeForm } from "../../modules/auth";
+import { changeField, initializeForm, register } from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
+import { check } from "../../modules/user";
 import { withRouter } from "react-router-dom";
 
-const RegisterForm = () => {
+const RegisterForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({ form: auth.register }));
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user
+  }));
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -22,20 +28,37 @@ const RegisterForm = () => {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    const { email, password, passwordConfirm } = form;
+    const { username, email, password, passwordConfirm } = form;
     if (password !== passwordConfirm) {
       return;
     }
-    // dispatch()
-    // let loginForm = {
-    //   email: Email,
-    //   password: Password
-    // };
+    dispatch(register({ username, email, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm("register"));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log("오류 발생");
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log("회원가입 성공");
+      console.log(auth);
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      console.log("check API 성공");
+      console.log(user);
+      history.push("/");
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
