326ff820061cb9b8d2fce3d35caadf7e7c0d370c
Redonearth <wona23@gmail.com>
2021-02-19 01:02:28 +0900

    Schema 수정

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,8 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
-const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   username: String,
